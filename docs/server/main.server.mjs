3da6fe59a1361b03204ea196fd062d70d9177381
Hezron Murith <jeihezmurithi@gmail.com>
2025-06-10 11:19:09 +0300

    updated the docs file

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-JOG7SIS4.mjs";import"./chunk-UKX7EYJ7.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-D2BFMHJA.mjs";import"./chunk-UKX7EYJ7.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
